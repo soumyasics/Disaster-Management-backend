5a7d81971de200e1355e58adedc0e2aab742a41e
Beninsha27 <beninsharb@gmail.com>
2024-06-18 17:43:56 +0530

    dms

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express=require ("express")
 const router=express.Router()
 
 const user=require("./models/user/userController")
+const volunteers=require("./models/volunteers/volunteersController")
 
 router.post("/registeruser",user.upload,user.registeruser)
 router.post("/userlogin",user.userlogin)
@@ -11,5 +12,11 @@ router.post("/viewuserbyid/:id",user.viewuserbuid)
 router.post("/editusrprofile/:id",user.editUserById)
 router.post("/deleteuser/:id",user.deleteUserById)
 
+router.post("/registervolunteer",volunteers.registervolunteers)
+router.post("/volunteerlogin",volunteers.volenteerslogin)
+router.post("/volunteerforgotpassword",volunteers.forgotPwd)
+router.post("/viewalladvocates",volunteers.viewallvolenteers)
+router.post("/viewvolunteerById/:id",volunteers.viewvolenteerById)
+router.post("/deletevolunteerBy/:id",volunteers.deletevolenteerById)
 
 module.exports=router
